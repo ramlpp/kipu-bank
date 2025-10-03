18ad9f2edf187ee23d89d73c778c4a9813fe643e
ramlpp <ramiro_luppi@hotmail.com>
2025-10-03 14:58:52 -0300

    Añadido eventos

--- contracts/KipuBank.sol
+++ contracts/KipuBank.sol
@@ -4,11 +4,12 @@ pragma solidity 0.8.30;
 /**
  * @title KipuBank - Bóveda de depósito y retiro de ETH con límites
  * @author Dev ramlpp
- * @notice Permite a los usuarios depositar y retirar ETH bajo ciertas restricciones
+ * @notice Permite a los usuarios depositar y retirar ETH bajo ciertos parametros
  * @dev Este contrato sigue buenas prácticas de seguridad y documentación en Solidity
  */
 contract kipubank {
 
+    // ──────── VARIABLES INMUTABLES ────────
     /// @notice Límite máximo de retiro por transacción
     /// @dev Se define en el constructor y no puede modificarse
     uint256 public immutable withdrawLimitPerTx;
@@ -17,6 +18,7 @@ contract kipubank {
     /// @dev Se define en el constructor y no puede modificarse
     uint256 public immutable depositLimitPerTx;
 
+    // ──────── VARIABLES DE ESTADO ────────
     /// @notice Mapa que almacena el saldo de ETH de cada dirección
     mapping(address => uint256) private _balances;
 
@@ -31,6 +33,11 @@ contract kipubank {
 
     
     // ──────── EVENTOS ────────
+    /// @notice Evento emitido cuando un usuario deposita
+    event Deposit(address indexed user, uint256 amount, uint256 balance, uint256 totalDeposited);
+
+    /// @notice Evento emitido cuando un usuario retira
+    event Withdraw(address indexed user, uint256 amount, uint256 balance, uint256 totalDeposited);
 
     // ──────── CONSTRUCTOR ────────
 
